26418cab270793cc999f61143523f9eae0e047b6
Patrick <phuan097@uottawa.ca>
2023-07-04 17:28:50 -0400

    <feature> Implemented Our Core Values section of About page

--- components/data.js
+++ components/data.js
@@ -1,6 +1,6 @@
 export const data = {
   nav: {
-    titles: ["HOME", "ABOUT", "PROJECTS", "JOIN US", "CONTACT US"],
+    titles: ["HOME", "ABOUT", "PROJECTS", "JOIN US", "CONTACT"],
     links: ["", "about", "projects", "join", "contact"],
   },
   home: {
@@ -11,18 +11,23 @@ export const data = {
   },
   about: {
     aboutUs:
-      "Cerulean non-profit organization was initially founded in 2020 to unite the French, English and Vietnamese cultures. To date, Cerulean has successfully connected more than 300 participants to English and French volunteer tutors from Vietnam and abroad. Noticing how impactful education is on the future generation, Cerulean strongly believes that providing quality education for all is fundamental to creating a peaceful and prosperous world. The organization's new mission is to support all children across Vietnam to access a quality and inclusive education. Cerulean will be working to provide quality learning opportunities that prepare Vietnamese children with the knowledge and skill they need to acquire a healthier and better life.",
+      "Cerulean non-profit organization was initially founded in 2020 to unite the French, English and Vietnamese cultures. To date, Cerulean has successfully connected more than 300 participants to English and French volunteer tutors from Vietnam and abroad. Noticing how impactful education is on the future generation, Cerulean strongly believes that providing quality education for all is fundamental to creating a peaceful and prosperous world. The organization's new mission is to support all children across Vietnam to access a quality and inclusive education. Cerulean will be working to provide quality learning opportunities that prepare Vietnamese children with the knowledge and skill they need to acquire a healthier and better life. ______ Cerulean Organization",
+    subtitles: {
+      core: "Our Core Values",
+      story: "Our Story",
+      team: "Meet Our Team",
+    },
     ourCoreValues: [
       [
-        "Care",
+        "CARE",
         "We help each other to develop in an inclusive environment where everyone feels heard, understood and appreciated.",
       ],
       [
-        "Respect",
+        "RESPECT",
         "We treat each other with the same level of respect we want for ourselves, as we promote inclusion, diversity, compassion and a sense of belonging for all.",
       ],
       [
-        "Empowerment",
+        "EMPOWERMENT",
         "We value each other's abilities, goals and learning. We encourage and support all to exhibit a sense of independence and freedom.",
       ],
     ],
--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,32 @@
 import Head from "next/head";
 import { data } from "../components/data.js";
+import AboutCSS from "../styles/About.module.css";
 
 const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | About</title>
+        <title>Cerulean | About</title>
         <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        <h1>About Us</h1>
-        <p>{data.about.aboutUs}</p>
-        {data.about.ourCoreValues.map((valueList) => {
-          return (
-            <div>
-              <h2>{valueList[0]}</h2>
-              <p>{valueList[1]}</p>
-            </div>
-          );
-        })}
+        <div className={AboutCSS.title}>About Us</div>
+        <div className={AboutCSS.description}>{data.about.aboutUs}</div>
+        <div className={AboutCSS.subtitle}>{data.about.subtitles.core}</div>
+        <div className={AboutCSS.valuecontainer}>
+          <div className={AboutCSS.valuegrid}>
+            {data.about.ourCoreValues.map((valueList) => {
+              return <div className={AboutCSS.value}>{valueList[0]}</div>;
+            })}
+            {data.about.ourCoreValues.map((valueList) => {
+              return (
+                <div className={AboutCSS.valuedescription}>{valueList[1]}</div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={AboutCSS.subtitle}>{data.about.subtitles.story}</div>
+        <div className={AboutCSS.subtitle}>{data.about.subtitles.team}</div>
       </div>
     </>
   );
