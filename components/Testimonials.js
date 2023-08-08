efeab5c62a49fa5f5e3441018a82cafb78dd90d9
Patrick <phuan097@uottawa.ca>
2023-08-07 22:36:59 -0400

    <feature> Implemented Framer Motion library for fading upon auto-scrolling

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,20 +1,35 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import TestimonialsCSS from "../styles/Testimonials.module.css";
 import { data } from "./data";
-import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 const Testimonials = ({ testimonialsCounter = 0, setTestimonialsCounter }) => {
   const [seconds, setSeconds] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const show = {
+    opacity: 1,
+  };
+
+  const hide = {
+    opacity: 0,
+  };
 
   useEffect(() => {
     let interval = null;
 
+    setVisible(true);
+
     if (seconds == 6) {
-      setSeconds(0);
-      setTestimonialsCounter(
-        (testimonialsCounter + 1) % data.about.testimonialsText.length
-      );
-      clearInterval(interval);
+      setVisible(false);
+      setTimeout(() => {
+        setSeconds(0);
+        setTestimonialsCounter(
+          (testimonialsCounter + 1) % data.about.testimonialsText.length
+        );
+        clearInterval(interval);
+      }, 500);
     } else {
       console.log(seconds);
       interval = setInterval(() => {
@@ -37,24 +52,33 @@ const Testimonials = ({ testimonialsCounter = 0, setTestimonialsCounter }) => {
           src="/left_icon.png"
           width={12}
           height={22}
-          onClick={() =>
+          onClick={() => {
+            setVisible(false);
             setTestimonialsCounter(
               (testimonialsCounter - 1 + data.about.testimonialsText.length) %
                 data.about.testimonialsText.length
-            )
-          }
+            );
+          }}
         />
-        <div className={TestimonialsCSS.text}>
+        <motion.div
+          className={TestimonialsCSS.text}
+          animate={visible ? show : hide}
+        >
           <div>{data.about.testimonialsText[testimonialsCounter][0]}</div>
           <div className={TestimonialsCSS.name}>
             {data.about.testimonialsText[testimonialsCounter][1]}
           </div>
-        </div>
+        </motion.div>
         <Image
           src="/right_icon.png"
           width={12}
           height={22}
-          onClick={() => setTestimonialsCounter((testimonialsCounter + 1) % 7)}
+          onClick={() => {
+            setVisible(false);
+            setTestimonialsCounter(
+              (testimonialsCounter + 1) % data.about.testimonialsText.length
+            );
+          }}
         />
       </div>
     </div>
