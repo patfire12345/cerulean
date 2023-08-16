2c96fe967314e89a3114a3cfd07c7183ecc8097e
Patrick <phuan097@uottawa.ca>
2023-08-16 19:32:27 -0400

    <feature> Implemented email subscription popup

--- components/SubscribePopup.js
+++ components/SubscribePopup.js
@@ -1,10 +1,49 @@
+import { useState } from "react";
 import Popup from "reactjs-popup";
+import PopupCSS from "../styles/SubscribePopup.module.css";
+import Image from "next/image";
 
 export default function SubscribePopup({ children }) {
+  const [open, setOpen] = useState(true);
   return (
     <div>
-      <Popup trigger={children} modal>
-        Hello
+      <Popup trigger={children} open={open} modal>
+        {(close) => (
+          <div className={PopupCSS.backgroundcontainer}>
+            <div className={PopupCSS.modalcontainer}>
+              <Image
+                onClick={() => {
+                  close();
+                }}
+                src="/X.png"
+                width={15}
+                height={17}
+                className={PopupCSS.X}
+              />
+              <div className={PopupCSS.modaltitle}>
+                STAY UP TO DATE WITH OUR WORK!
+              </div>
+              <div className={PopupCSS.modaltext}>
+                Want to know what we're up to? Sign up for our email list here!
+              </div>
+              <div className={PopupCSS.subscribecontainer}>
+                <input
+                  placeholder="Email address"
+                  className={PopupCSS.modalinput}
+                />
+                <div
+                  className={PopupCSS.button}
+                  onClick={() => {
+                    console.log("Subscribed to newsletter!");
+                    close();
+                  }}
+                >
+                  Subscribe
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </Popup>
     </div>
   );
