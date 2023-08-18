1a652aaff96a7ac5ddcd9123d79753066dee509b
Patrick <phuan097@uottawa.ca>
2023-08-17 23:40:33 -0400

    <update> Cleaned up Footer code by creating a loginComponent

--- components/Footer.js
+++ components/Footer.js
@@ -57,82 +57,21 @@ const Footer = ({ EN }) => {
     }
   };
 
-  if (session) {
-    return (
-      <div className={FooterCSS.container}>
-        <div className={FooterCSS.footercontainer}>
-          <div className={FooterCSS.explorecontainer}>
-            <div className={FooterCSS.footertitle}>
-              {data.footer.explore[0]}
-            </div>
-            <div className={FooterCSS.exploreitemscontainer}>
-              {data.footer.titles.map((title, index) => {
-                if (data.footer.links[index] != "contact") {
-                  return (
-                    <Link
-                      href={"/".concat(data.footer.links[index])}
-                      className={FooterCSS.exploreitem}
-                    >
-                      {title}
-                    </Link>
-                  );
-                }
-              })}
-            </div>
-          </div>
-          <div className={FooterCSS.portalcontainer}>
-            <div className={FooterCSS.footertitle}>{data.footer.portal[0]}</div>
-            <div className={FooterCSS.portalitem} onClick={() => signOut()}>
-              {data.footer.portal[2]}
-            </div>
-          </div>
-          <div className={FooterCSS.newslettercontainer}>
-            <div className={FooterCSS.footertitle}>
-              {data.footer.newsletter[0]}
-            </div>
-            <div className={FooterCSS.newsletteritem}>
-              {data.footer.newsletter[1]}
-            </div>
-            <div className={FooterCSS.newsletterform}>
-              <input
-                className={FooterCSS.newsletterinput}
-                placeholder="Email address"
-              />
-              <ConfirmPopup EN={EN}>
-                <div className={FooterCSS.newsletterbutton}>Subscribe</div>
-              </ConfirmPopup>
-            </div>
-          </div>
-          <div className={FooterCSS.contactcontainer}>
-            <div className={FooterCSS.footertitle}>
-              {data.footer.contact[0]}
-            </div>
-            <Link href="/contact" className={FooterCSS.contactitem}>
-              {data.footer.contact[1]}
-            </Link>
-          </div>
-          <div className={FooterCSS.iconscontainer}>
-            {data.contact.links.map((link, index) => {
-              return (
-                <div className={FooterCSS.icon}>
-                  <Link href={link}>
-                    <Image
-                      src={data.footer.icons[index]}
-                      width={41}
-                      height={41}
-                    />
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-          <div className={FooterCSS.copyrighttext}>
-            {data.footer.copyright[0]}
-          </div>
+  const loginComponent = () => {
+    if (session) {
+      return (
+        <div className={FooterCSS.portalitem} onClick={() => signOut()}>
+          {data.footer.portal[2]}
         </div>
-      </div>
-    );
-  }
+      );
+    } else {
+      return (
+        <div className={FooterCSS.portalitem} onClick={() => signIn()}>
+          {data.footer.portal[1]}
+        </div>
+      );
+    }
+  };
   return (
     <div className={FooterCSS.container}>
       <div className={FooterCSS.footercontainer}>
@@ -155,9 +94,7 @@ const Footer = ({ EN }) => {
         </div>
         <div className={FooterCSS.portalcontainer}>
           <div className={FooterCSS.footertitle}>{data.footer.portal[0]}</div>
-          <div className={FooterCSS.portalitem} onClick={() => signIn()}>
-            {data.footer.portal[1]}
-          </div>
+          {loginComponent()}
         </div>
         <div className={FooterCSS.newslettercontainer}>
           <div className={FooterCSS.footertitle}>
